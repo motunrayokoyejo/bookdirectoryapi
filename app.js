e37815a42d12cfe663967ac8933d365a91fca299
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2021-02-19 15:15:50 +0100

    'hashedpasswords'

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser =  require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 
-const bookRoutes = require('./api/routes/books')
+const bookRoutes = require('./api/routes/books');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect('mongodb+srv://motun:motunrayo@node-restapi.uxluz.mongodb.net/booksDB?retryWrites=true&w=majority',
-{
+{ 
+    
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected....'))
 .catch(err => console.log(err));
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+
 app.use('/books', bookRoutes);
+app.use('/users', userRoutes);
+
+// app.use((req, res, next) => {
+//     const error = new Error("Not found");
+//     error.status = 404;
+//     next(error);
+//   });
+  
+//   app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     res.json({
+//       error: {
+//         message: error.message
+//       }
+//     });
+//   });
 
 
 module.exports = app;
